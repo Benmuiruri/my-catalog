d55275507a951d6366a7ddccfc6dd22ba11fc2bd
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-27 20:47:38 +0300

    Rename create service to add service

---
+++ add_service.rb
@@ -0,0 +1,22 @@
+def add_service(option)
+  case option
+  when 3
+    add_book
+  when 4
+    add_music_album
+  when 5
+    add_game
+  end
+end
+
+def add_book
+  puts "This is for adding a book"
+end
+
+def add_music_album
+  puts "This is for adding a music album"
+end
+
+def add_game
+  puts "This is for adding a game"
+end
