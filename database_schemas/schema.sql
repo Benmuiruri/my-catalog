8df49ecbb3cd71f5e34dead1d27bfb5d5c897afd
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-28 12:49:53 +0300

    Fix foreign key conflict

--- database_schemas/schema.sql
+++ database_schemas/schema.sql
@@ -4,31 +4,22 @@ CREATE TABLE items(
   author_id SERIAL NOT NULL,
   label_id SERIAL NOT NULL,
   genre_id SERIAL NOT NULL,
-  publish_date TIMESTAMP NOT NULL,
-  CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES author(id),
-  CONSTRAINT label_fk FOREIGN KEY (label_id) REFERENCES label(id),
-  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genre(id)
+  publish_date TIMESTAMP NOT NULL
 );
-
-
 CREATE TABLE genre(
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50),
   item_id SERIAL NOT NULL,
   CONSTRAINT item_fk FOREIGN KEY (item_id) REFERENCES items(id)
 );
-
-
-CREATE TABLE music_album ( 
+CREATE TABLE music_album (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50),
   publish_date DATE,
   archived BOOLEAN,
-  on_spotify BOOLEAN DEFAULT 'f'
-  genre_id INT
+  on_spotify BOOLEAN DEFAULT 'f',
+  genre_id INT,
   CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genre(id)
-)
-
-CREATE INDEX genre_id_asc ON music_album(genre_id ASC);
-Create INDEX genre_id_asc ON items(genre_id ASC);
-
+);
+CREATE INDEX genre_id_asc_music ON music_album(genre_id ASC);
+Create INDEX genre_id_asc_items ON items(genre_id ASC);
