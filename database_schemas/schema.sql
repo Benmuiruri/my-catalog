7f8b83e23a32d5f50ec11e311412583e78db2552
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-28 13:03:20 +0300

    Update Schema

--- database_schemas/schema.sql
+++ database_schemas/schema.sql
@@ -1,25 +1,16 @@
-CREATE TABLE items(
-  id SERIAL PRIMARY KEY,
-  archived BOOLEAN DEFAULT 'f',
-  author_id SERIAL NOT NULL,
-  label_id SERIAL NOT NULL,
-  genre_id SERIAL NOT NULL,
-  publish_date TIMESTAMP NOT NULL
-);
-CREATE TABLE genre(
+CREATE TABLE genres(
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50),
-  item_id SERIAL NOT NULL,
-  CONSTRAINT item_fk FOREIGN KEY (item_id) REFERENCES items(id)
+  items TEXT []
 );
-CREATE TABLE music_album (
+CREATE TABLE music_albums (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50),
   publish_date DATE,
   archived BOOLEAN,
   on_spotify BOOLEAN DEFAULT 'f',
   genre_id INT,
-  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genre(id)
+  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
-CREATE INDEX genre_id_asc_music ON music_album(genre_id ASC);
-Create INDEX genre_id_asc_items ON items(genre_id ASC);
+
+CREATE INDEX genre_id_asc ON music_albums(genre_id ASC);
