f8351a0a6795d5674e9d4b2804f7035f458096e5
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-28 12:26:53 +0300

    rename schema file and add create ITEM table sql

--- database_schemas/schema.sql
+++ database_schemas/schema.sql
@@ -1,10 +1,23 @@
+CREATE TABLE items(
+  id SERIAL PRIMARY KEY,
+  archived BOOLEAN DEFAULT 'f',
+  author_id SERIAL NOT NULL,
+  label_id SERIAL NOT NULL,
+  genre_id SERIAL NOT NULL,
+  publish_date TIMESTAMP NOT NULL,
+  CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES author(id),
+  CONSTRAINT label_fk FOREIGN KEY (label_id) REFERENCES label(id),
+  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genre(id)
+);
+
+
 CREATE TABLE genre(
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50),
   item_id SERIAL NOT NULL,
   CONSTRAINT item_fk FOREIGN KEY (item_id) REFERENCES items(id)
 );
-CREATE INDEX item_id_asc ON musicalbum(item_id ASC);
+
 
 CREATE TABLE music_album ( 
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -15,6 +28,7 @@ CREATE TABLE music_album (
   genre_id INT
   CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genre(id)
 )
-Create INDEX genre_id_asc ON items(genre_id ASC);
+
 CREATE INDEX genre_id_asc ON musicalbum(genre_id ASC);
+Create INDEX genre_id_asc ON items(genre_id ASC);
 
