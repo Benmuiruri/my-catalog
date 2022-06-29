b4f5ff1a46f66afaf90ad524aa49c1c76fc3fd19
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-29 16:51:12 +0300

    Refactor add_music_album

--- modules/music_album_services.rb
+++ modules/music_album_services.rb
@@ -5,33 +5,58 @@ require 'pry'
 
 module MusicAlbumServices
   include SaveData
-  def add_music_album
-    correct_input = false
-    genre_input = ''
-    print 'Enter the name of the album:'
-    album_name = gets.chomp
-    print 'Enter the name of the artist: '
-    artist_name = gets.chomp
-    print 'Enter music album publish date (mm/dd/yyyy): '
-    publish_date = gets.chomp
 
-    while genre_input.empty?
-      print 'Enter genre of the music album: '
-      genre_input = gets.chomp
-      puts 'Genre name cannot be blank, please enter a valid genre name' if genre_input.empty?
+  def music_album_data(input)
+    output = ''
+    while output.empty?
+      print "Enter the #{input} of the music album: "
+      output = gets.chomp
+      puts "#{input} cannot be blank, please enter a valid #{input} " if output.empty?
     end
+    output
+  end
 
+  def music_album_publish_date(input)
+    output = ''
+    while output.empty?
+      print "Enter the #{input} of the music album: "
+      output = gets.chomp
+      puts "#{input} cannot be blank, please enter a valid #{input} " if output.empty?
+    end
+    output
+  end
+
+  def spotify_listing
+    correct_input = false
     until correct_input
       print 'Is the album on Spotify [Y/N]:'
       on_spotify_input = gets.chomp.downcase
       correct_input = %w[y n].include?(on_spotify_input)
       puts "Please enter Y or N only\n"
     end
+    _on_spotify = on_spotify_input == 'y'
+  end
 
-    on_spotify = on_spotify_input == 'y'
+  def genre_data(input)
+    output = ''
+    while output.empty?
+      print "Enter #{input} of the music album: "
+      output = gets.chomp
+      puts "#{input}  cannot be blank, please enter a valid #{input} " if output.empty?
+    end
+    output
+  end
+
+  def add_music_album
+    album_name = music_album_data('Name')
+    artist_name = music_album_data('Artist Name')
+    publish_date = music_album_publish_date('Publish Date')
+    genre_name = genre_data('Genre')
+    on_spotify = spotify_listing
+
     music_album = MusicAlbum.new(album_name, artist_name, publish_date, on_spotify)
-    puts "The music album #{album_name} created successfully"
-    genre = Genre.new(genre_input)
+    puts "The music album #{album_name} was created successfully"
+    genre = Genre.new(genre_name)
     music_album.add_genre(genre)
     SaveData.save_book
   end
