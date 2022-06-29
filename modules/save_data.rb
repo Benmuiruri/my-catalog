d22196386add351b953ccb4574bcdf684123b68e
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-29 17:10:59 +0300

    Fix data overriding bug on save data

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -11,7 +11,11 @@ module SaveData
   end
 
   def self.save_music_albums(music_albums)
-    music_albums_array = music_albums.map do |music_album|
+    File.write('./json_files/music_albums.json', '[]') unless File.exist?('./json_files/music_albums.json')
+    music_albums_array = JSON.parse(File.read('./json_files/music_albums.json'))
+
+    music_albums.map do |music_album|
+      music_albums_array <<
     { class_instance: 'MusicAlbum', id: music_album.id, album_name: music_album.album_name, artist_name: music_album.artist_name, publish_date: music_album.publish_date, on_spotify: music_album.on_spotify, genre: music_album.genre.name }
   end
   File.write('./json_files/music_albums.json', JSON.generate(music_albums_array))
