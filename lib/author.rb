cfce107c1c9f12735f84fc15563e7515b3bed797
keza681 <lindakeza19@gmail.com>
2022-06-30 21:41:44 +0200

    Correct author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,19 +1,15 @@
 class Author
-  attr_accessor :first_name, :last_name
-  attr_reader :id, :items
-
+  attr_accessor :first_name, :last_name, :items
+​
   def initialize(first_name, last_name)
     @id = rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
-
-  def add_item(item)
-    @items << item
-    item.author = self
+​
+  def add_items(game)
+    @items.push(game)
+    game.add_author(self)
   end
 end
-
-author = Author.new('Linda', 'Keza')
-puts author
