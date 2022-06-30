73618dc645ffaefbe491b834d7804c05075f3505
keza681 <lindakeza19@gmail.com>
2022-06-30 21:44:18 +0200

    Implement the game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,16 @@
 require_relative './item'
-
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
-
-  def initialize(multiplayer, last_played_at, publish_date)
+  attr_accessor :name, :multiplayer, :last_played_at
+  def initialize(name, multiplayer, last_played_at, publish_date)
     super(publish_date)
+    @name = name
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Date.strptime(last_played_at, '%Y-%m-%d')
   end
-
-  private
-
+  # private
   def can_be_archived?
     current_year = Time.new.year
-    seniority = current_year - @last_played_at
+    seniority = current_year - @last_played_at.year
     super && seniority > 2
   end
 end
